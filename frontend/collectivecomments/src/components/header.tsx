658a17f1ae1a4e38a68e0ed335ff0cde8fe7c562
Sara Romao <sara-romao@live.com>
2025-02-08 22:14:46 -0300

    feat: create menu

--- frontend/collectivecomments/src/components/header.tsx
+++ frontend/collectivecomments/src/components/header.tsx
@@ -1,17 +1,66 @@
+"use client"; // Indica que este é um Client Component
+
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation"; // Substitui o useRouter
 
+export default function Header() {
+  const pathname = usePathname(); // Obtém a rota atual
 
-export default function Header(){
+  // Função para verificar se o link está ativo
+  const isActive = (href: string) => pathname === href;
 
-    return(
-        <header className="flex justify-center p-3 border-b">
-            <Image
-                src={"/assets/logo.svg"}
-                alt="Logos"
-                width={235}
-                height={101}
-                priority
-            />
-        </header>
-    )
+  return (
+    <header className="flex items-center flex-col gap-8 p-3">
+      <Image
+        src={"/assets/logo.svg"}
+        alt="Logo"
+        width={235}
+        height={101}
+        priority
+      />
+
+      <nav>
+        <ul className="flex gap-5">
+          <li>
+            <Link
+              href="/"
+              className={`text-lg font-medium ${
+                isActive("/")
+                  ? "text-indigo-600 underline" 
+                  : "text-gray-700 hover:text-indigo-500" 
+              }`}
+            >
+              Criar Código
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/send-feedback"
+              className={`text-lg font-medium ${
+                isActive("/send-feedback")
+                  ? "text-indigo-600 underline" 
+                  : "text-gray-700 hover:text-indigo-500"
+              }`}
+            >
+              Enviar Feedback
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/list-feedbacks"
+              className={`text-lg font-medium ${
+                isActive("/list-feedbacks")
+                  ? "text-indigo-600 underline" 
+                  : "text-gray-700 hover:text-indigo-500"
+              }`}
+            >
+              Listar Feedback
+            </Link>
+          </li>
+       
+        </ul>
+      </nav>
+    </header>
+  );
 }
