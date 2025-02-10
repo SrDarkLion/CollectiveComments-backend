2d8f6a3574295b47248beebb1a9831d4c91c0e77
Sara Romao <sara-romao@live.com>
2025-02-09 22:18:48 -0300

    feat: adjustment in form fields

--- frontend/collectivecomments/src/app/page.tsx
+++ frontend/collectivecomments/src/app/page.tsx
@@ -1,6 +1,7 @@
 
 
 export default function Home() {
+  
   return (
     <div className="flex justify-center pt-5">
       <div className="mx-auto max-w-xl w-full px-4 sm:px-6 lg:px-8">
@@ -9,36 +10,39 @@ export default function Home() {
           <form action="#" className="space-y-4 border rounded-md p-4 sm:p-6 lg:p-8">
           
             <div className="flex flex-col gap-2">
-              <label htmlFor="email" className="font-semibold">Nome da Empresa</label>
+              <label htmlFor="name" className="font-semibold">Nome da Empresa</label>
 
               <div className="relative">
                 <input
                   type="text"
-                  className="w-full rounded-lg border border-gray-200 p-4 pe-12 text-base shadow-xs"
+                  className="w-full rounded-lg border border-gray-200 p-2 pe-12 text-base shadow-xs"
+                  name="name"
                   placeholder=""
                 />
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
-              <label htmlFor="email" className="font-semibold">Senha</label>
+              <label htmlFor="password" className="font-semibold">Senha</label>
 
               <div className="relative">
                 <input
                   type="password"
-                  className="w-full rounded-lg border border-gray-200 p-4 pe-12 text-base shadow-xs"
+                  className="w-full rounded-lg border border-gray-200 p-2 pe-12 text-base shadow-xs"
+                  name="password"
                   placeholder=""
                 />
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
-              <label htmlFor="email" className="font-semibold">Repetir Senha</label>
+              <label htmlFor="repeatpassoword" className="font-semibold">Repetir Senha</label>
 
               <div className="relative">
                 <input
                   type="password"
-                  className="w-full rounded-lg border border-gray-200 p-4 pe-12 text-base shadow-xs"
+                  className="w-full rounded-lg border border-gray-200 p-2  pe-12 text-base shadow-xs"
+                  name="repeatpassoword"
                   placeholder=""
                 />
               </div>
@@ -53,16 +57,18 @@ export default function Home() {
           </form>
 
           <div className="sm:p-6 lg:p-8">
-            <div className="flex justify-between gap-3">
-                <div className="w-full">
+            <div className="flex justify-between items-end gap-3">
+                <div className="w-full flex flex-col gap-2">
+                  <label className="mt-1 font-semibold">Código Gerado</label>
                   <input
-                    type="password"
-                    className="w-full rounded-lg border border-gray-200 p-2  text-base shadow-xs"
-                    placeholder=""
+                    type="codefeedback"
+                    className="w-full rounded-lg border h-11 border-gray-200 p-2  text-base shadow-xs"
+                    disabled
+                    value={'coca-2221'}
                   />
                 </div>
 
-                <button className="block w-20 rounded-lg bg-indigo-600 px-5 p-0 text-sm font-medium text-white">Copiar</button>
+                <button className="block w-20 rounded-lg bg-indigo-600 px-5 h-11 text-sm font-medium text-white">Copiar</button>
             </div>
             <p className="mt-1 text-red-500 font-semibold">Observação: Salvar código em um local seguro  </p>
               
